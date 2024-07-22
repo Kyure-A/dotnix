3b8c4e30e537a87b97c6d87bd5a4f73f32cc08e8
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-07-22 13:19:58 +0000

    refactor: modify configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,11 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
-
-{
-  # Bootloader.
+{ config, pkgs, ... }: {
   boot = {
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
@@ -58,65 +51,50 @@
       };
     };
   };  
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.windowManager.exwm = {
-    enable = true;
-  
-  };
-   
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+  services = {
+    xserver = {
+      enable = true;
+      displayManager = {
+        gdm.enable = true;
+      };
+      # desktopManager.gnome.enable = true;
+      # windowManager.exwm.enable = true;
+      layout = "us";
+      xkbVariant = "";
+    };
+    printing.enable = true;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
   };
-
+  
   users.users.kyre = {
     isNormalUser = true;
-    description = "kyre";
     extraGroups = [ "networkmanager" "wheel" ];
+    defaultUserShell = pkgs.zsh;  
     packages = with pkgs; [
-    ];
+      # add package below
+    ];    
   };
 
-  nixpkgs.config.allowUnfree = true;
-
-  programs.zsh.enable = true; 
-
-  users.defaultUserShell = pkgs.zsh;  
+  programs = {
+    zsh.enable = true; 
+  };
   
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     # ((emacsPackagesFor emacsPgtkNativeComp).emacsWithPackages (
     #   epkgs: [ epkgs.vterm ]
     # ))
   ];
- system.stateVersion = "24.05"; # Did you read the comment?
-
+  
+  nixpkgs.config.allowUnfree = true;
+  system.stateVersion = "24.05";
 }
