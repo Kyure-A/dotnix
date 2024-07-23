7d85492c4648ede73f867cd6a3b12f3fd15dc560
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-07-24 05:06:10 +0900

    fix

--- flake.nix
+++ flake.nix
@@ -2,16 +2,16 @@
   description = "Kyure_A's NixOS Config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     emacs-overlay.url = "github:nix-community/emacs-overlay";
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL";
-      inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = { self, nixpkgs, emacs-overlay, nixos-wsl }: {
-    nixos-wsl-config = (import ./systems/wsl {
+    nixosConfigurations = (import ./systems/wsl {
       inherit self nixpkgs emacs-overlay nixos-wsl;
     });
   };
