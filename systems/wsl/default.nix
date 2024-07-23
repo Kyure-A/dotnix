60380e8d995b1d94c1f48388d5c4c9c15066cd81
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-07-24 05:04:09 +0900

    feat: add specialArgs

--- systems/wsl/default.nix
+++ systems/wsl/default.nix
@@ -1,8 +1,12 @@
 { self, nixpkgs, emacs-overlay, nixos-wsl }: {
   wsl = nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
     modules = [
-      ../../nixos-wsl
+      ../../nixos-wsl/default.nix
       nixos-wsl.nixosModules.default
     ];
+    specialArgs = {
+      nixos-wsl = nixos-wsl;
+    };
   };
 }
