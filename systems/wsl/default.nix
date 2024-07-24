c363baaed5f9616d908c0cbb6351f28f0d39a539
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-07-24 19:19:08 +0900

    refactor: rewrite by let statement

--- systems/wsl/default.nix
+++ systems/wsl/default.nix
@@ -1,12 +1,16 @@
-{ self, nixpkgs, home-manager, nixos-wsl, emacs-overlay }: {
-  wsl = nixpkgs.lib.nixosSystem rec {
-    system = "x86_64-linux";
+{ self, nixpkgs, home-manager, nixos-wsl, emacs-overlay }:
+let
+  username = "kyre";
+  system = "x86_64-linux";
+in {
+  wsl = nixpkgs.lib.nixosSystem {
+    inherit system;
     modules = [
       ../../nixos-wsl
       nixos-wsl.nixosModules.default
       home-manager.nixosModules.home-manager {
         home-manager.useUserPackages = true;
-        home-manager.users.kyre = import ../../home { inherit system nixpkgs emacs-overlay; };
+        home-manager.users."${username}" = import ../../home { inherit system nixpkgs emacs-overlay; };
       }
     ];
     specialArgs = {
