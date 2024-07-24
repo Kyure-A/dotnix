d32b40f0ab11b1c1d8db12b5aecaeab91ab1c1d3
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-07-24 18:50:33 +0900

    feat: add git and starship settings

---
+++ home/programs/git/default.nix
@@ -0,0 +1,44 @@
+{
+  programs.git = {
+    enable = true;
+
+    user = {
+      name = "Kyure_A";
+      email = "49436968+Kyure-A@users.noreply.github.com";
+    };
+
+    extraConfig = {
+      core = {
+        editor = "emacs -nw";
+        quotepath = false;
+        pager = "delta";
+      };
+
+      github = {
+        user = "Kyure-A";
+      };
+
+      init = {
+        defaultBranch = "master";
+      };
+
+      merge = {
+        conflictstyle = "diff3";
+      };
+
+      diff = {
+        colorMoved = "default";
+      };
+
+      interactive = {
+        diffFilter = "delta --color-only";
+      };
+
+      delta = {
+        side-by-side = true;
+        navigate = true;
+        light = false;
+      };
+    };
+  };
+}
---
+++ home/programs/starship/default.nix
@@ -0,0 +1,38 @@
+{
+  programs.starship = {
+    enable = true;
+    settings = {
+      battery = {
+        full_symbol = "🔋 ";
+        charging_symbol = "⚡️ ";
+        discharging_symbol = "💀 ";
+      };
+      cmd_duration = {
+        disabled = false;
+        min_time = 4000;
+      };
+      directory = {
+        disabled = false;
+        truncation_length = 100;
+        truncate_to_repo = false;
+      };
+      username = {
+        style_user = "white bold";
+        style_root = "yellow bold";
+        format = "[$user]($style)";
+        disabled = false;
+        show_always = true;
+      };
+      hostname = {
+        format = "@[$hostname](white) \n";
+        ssh_only = false;
+        disabled = false; 
+      };
+      time = {
+        disabled = false;
+        use_12hr = false;
+        format = "[\[🕙 $time \]]($style) ";
+      };
+    };
+  };
+}
