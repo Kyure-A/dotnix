cbad9e7656bd88b84f475ae24e3dc21161fe17ad
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-07-26 17:58:24 +0900

    refactor

--- home/programs/zsh/default.nix
+++ home/programs/zsh/default.nix
@@ -2,7 +2,11 @@
 let
   load-zsh-defer = ''source "${pkgs.zsh-defer}/share/zsh-defer/zsh-defer.plugin.zsh"'';
   load-sheldon = ''eval "$(sheldon source)"'';
-  text = load-zsh-defer + "\n" + load-sheldon;
+  text = builtins.concatStringsSep "\n"
+    [
+      load-zsh-defer
+      load-sheldon
+    ];
 in {
   home.file = {
     ".zshrc".source = pkgs.writeText ".zshrc" text;
