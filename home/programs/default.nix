e7ae7fda8b9bc8373d75001ec8275e86e7bfe723
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-07-26 17:12:07 +0900

    fix: sheldon config

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -1,9 +1,9 @@
 { pkgs }:
 let
   git = import ./git;
-  sheldon = import ./sheldon; #  { inherit pkgs; };
+  sheldon = import ./sheldon;
   starship = import ./starship;
-  zsh = import ./zsh;
+  zsh = import ./zsh { inherit pkgs; };
 in
 [ git
   sheldon
--- home/programs/zsh/default.nix
+++ home/programs/zsh/default.nix
@@ -1,6 +1,11 @@
-{
+{ pkgs, ... } :
+let
+  load-zsh-defer = ''source "${pkgs.zsh-defer}/share/zsh-defer/zsh-defer.plugin.zsh"'';
+  load-sheldon = ''eval "$(sheldon source)"'';
+  text = load-zsh-defer + "\n" + load-sheldon;
+in {
   home.file = {
-    ".zshrc".source = ./.zshrc;
+    ".zshrc".source = pkgs.writeText ".zshrc" text;
   };
   programs.zsh = {
     enable = true;
