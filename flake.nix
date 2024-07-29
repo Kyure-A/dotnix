85c47ac3bca97afbf70b37a6f1fc061c61591613
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-07-29 21:27:42 +0900

    style: format by nixfmt

--- flake.nix
+++ flake.nix
@@ -7,9 +7,7 @@
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    home-manager-kyre = {
-      url = "github:Kyure-A/home-manager/master";
-    };
+    home-manager-kyre = { url = "github:Kyure-A/home-manager/master"; };
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -17,18 +15,21 @@
     emacs-overlay.url = "github:nix-community/emacs-overlay";
   };
 
-  outputs = { self, nixpkgs, home-manager-official, home-manager-kyre, nixos-wsl, emacs-overlay }: let
-    settings = {
-      useOfficial = false;
-    };
-    home-manager = if settings.useOfficial then home-manager-official else home-manager-kyre;
-  in {
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt;
+  outputs = { self, nixpkgs, home-manager-official, home-manager-kyre, nixos-wsl
+    , emacs-overlay }:
+    let
+      settings = { useOfficial = false; };
+      home-manager = if settings.useOfficial then
+        home-manager-official
+      else
+        home-manager-kyre;
+    in {
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt;
 
-    nixosConfigurations = (import ./systems/wsl {
-      inherit self nixpkgs home-manager nixos-wsl emacs-overlay;
-    }) // (import ./systems/x230 {
-      inherit self nixpkgs home-manager emacs-overlay;
-    });
-  };
+      nixosConfigurations = (import ./systems/wsl {
+        inherit self nixpkgs home-manager nixos-wsl emacs-overlay;
+      }) // (import ./systems/x230 {
+        inherit self nixpkgs home-manager emacs-overlay;
+      });
+    };
 }
