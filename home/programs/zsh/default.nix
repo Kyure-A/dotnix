b6fa3f6fac494244967169f3d7c50b440f19e841
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-08-02 17:14:24 +0900

    feat(sheldon): enable completions script

--- home/programs/zsh/default.nix
+++ home/programs/zsh/default.nix
@@ -2,7 +2,12 @@
 let
   load-zsh-defer =
     ''source "${pkgs.zsh-defer}/share/zsh-defer/zsh-defer.plugin.zsh"'';
-  load-sheldon = ''eval "$(sheldon source)"'';
+  load-sheldon = ''
+    eval "$(${pkgs.sheldon} source)"
+    if [[ $TERM != "dumb" ]]; then
+       eval "$(${pkgs.sheldon} completions --shell=zsh)"
+    fi
+  '';
   text = builtins.concatStringsSep "\n" [ load-zsh-defer load-sheldon ];
 in {
   home.file = { ".zshrc".source = pkgs.writeText ".zshrc" text; };
