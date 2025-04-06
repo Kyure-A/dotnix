d48dd2e996d9d2798a7607b7f4e34b998120d9f7
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-04-06 23:19:08 +0900

    chore(darwin): add modules

--- systems/darwin/default.nix
+++ systems/darwin/default.nix
@@ -3,10 +3,24 @@
 let
   system = "aarch64-darwin";
   username = "kyre";
+  configuration = { ... }: {
+    users.users.${username}.home = "/Users/${username}";
+  };
 in {
   darwin = nix-darwin.lib.darwinSystem {
     inherit system;
     specialArgs = { inherit username; };
-    modules = [ ../../modules/nix-darwin ];
+    modules = [
+      configuration
+      ../../modules/nix-darwin
+      home-manager.darwinModules.home-manager
+      {
+        home-manager.useUserPackages = true;
+        home-manager.users."${username}" = import ../../home {
+          inherit system nixpkgs emacs-overlay rust-overlay fenix org-babel
+            rustowl-flake;
+        };
+      }
+    ];
   };
 }
