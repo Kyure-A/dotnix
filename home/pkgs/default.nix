adcfe028cc596247e184ee2808ad45df11749885
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-04-07 03:11:51 +0900

    chore(pkgs): add warp-terminal for darwin

--- home/pkgs/default.nix
+++ home/pkgs/default.nix
@@ -1,31 +1,37 @@
 { pkgs }:
-with pkgs; [
-  bun
-  delta
-  deno
-  docker
-  dotnet-sdk # C#
-  emacs-git
-  emacs-lsp-booster
-  eza
-  gcc
-  ghq
-  glance
-  gnumake
-  gnupg
-  keybase
-  neofetch
-  nixd
-  nixpkgs-fmt
-  nodePackages.typescript-language-server
-  omnisharp-roslyn # C#
-  openssl
-  pinentry-all
-  rust-bin.stable.latest.default
-  rust-analyzer
-  satysfi
-  # tectonic
-  tree-sitter
-  web-ext # mozilla
-  zsh-defer
-]
+with pkgs;
+let
+  common = [
+    bun
+    delta
+    deno
+    docker
+    dotnet-sdk # C#
+    emacs-git
+    emacs-lsp-booster
+    eza
+    gcc
+    ghq
+    glance
+    gnumake
+    gnupg
+    keybase
+    neofetch
+    nixd
+    nixpkgs-fmt
+    nodePackages.typescript-language-server
+    omnisharp-roslyn # C#
+    openssl
+    pinentry-all
+    rust-bin.stable.latest.default
+    rust-analyzer
+    satysfi
+    # tectonic
+    tree-sitter
+    web-ext # mozilla
+    zsh-defer
+  ];
+
+  nonDarwin = if !stdenv.isDarwin then [ pinentry-all ] else [ ];
+  darwin = if stdenv.isDarwin then [ warp-terminal ] else [ ];
+in common ++ nonDarwin ++ darwin
